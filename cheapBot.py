18e1d16a77d1cc9d593b8808439297fcfcec011a
Henistein <henriquejesus20@gmail.com>
2021-02-12 17:13:28 +0000

    cheap

--- cheapBot.py
+++ cheapBot.py
@@ -1,25 +1,47 @@
 import discord
 import re
 import os
+import datetime
 
 TOKEN = os.getenv('TOKEN')
 
 client = discord.Client()
 
+# insert the allowed channels
+allowed_channels = ['test']
+cooldown_list = []
+
+def allow_message(word, cooldow_list):
+  for i in cooldown_list:
+    if word in i:
+      if datetime.datetime.now() > i[1]:
+        cooldown_list.remove(i)
+        return True
+      else:
+        return False
+  return True
+
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+  print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+  if message.author == client.user:
+    return
 
-    if message.content:
-        aux = message.content.split()
-        for i in aux:
-            if re.search("^0x([A-Fa-f0-9]{40})$", i):
-              s = 'https://expedition.dev/address/' + i + '?rpcUrl=https%3A%2F%2Fnode.cheapeth.org%2Frpc'
-        await message.channel.send(s)
+  if message.content:
+    if message.channel.name in allowed_channels:
+      message_words = message.content.split()
+      for word in message_words:
+        if re.search("^0x([A-Fa-f0-9]{40})$", word):
+          s = ('**'+message.author.name+'**' + ': ' + 'https://expedition.dev/address/' + word + 
+              '?rpcUrl=https%3A%2F%2Fnode.cheapeth.org%2Frpc')
+
+          cooldown = (datetime.datetime.now() + datetime.timedelta(seconds=60))
+          if allow_message(word, cooldown_list):
+            cooldown_list.append((word, cooldown))
+            await message.channel.send(s)
 
 client.run(TOKEN)
