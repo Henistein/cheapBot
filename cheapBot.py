64e0a94b722182429d62523a3ba2ee33fd06fcdc
Henistein <henriquejesus20@gmail.com>
2021-02-16 23:22:14 +0000

    added API

--- cheapBot.py
+++ cheapBot.py
@@ -2,6 +2,7 @@ import discord
 import re
 import os
 import datetime
+import json
 from jsonrpcclient import request
 
 TOKEN = os.getenv('TOKEN')
@@ -10,7 +11,12 @@ client = discord.Client()
 
 # insert the allowed channels
 allowed_channels = ['test']
+
 cooldown_list = []
+meme_list = []
+
+global min_approval 
+min_approval = 10
 
 def allow_message(word, cooldow_list):
   for i in cooldown_list:
@@ -22,16 +28,50 @@ def allow_message(word, cooldow_list):
         return False
   return True
 
+def update_list(meme_list):
+  for i in meme_list:
+    i[3] = 0
+    for j in i[0].reactions:
+      i[3] += j.count
+    if datetime.datetime.now() > i[1]:
+      meme_list.remove(i)
+  return meme_list
+
+def update_json(meme_list):
+  temp = []
+  f = open("approval.json", "w")
+  f.close()
+  with open("approval.json", "w") as outfile:
+    for i in meme_list:
+      dictionary = {
+        "uuid" : "",
+        "reactions" : "",
+        "approval" : "",
+        "expired_time" : ""
+      }
+      dictionary["uuid"] = i[2]
+      dictionary["reactions"] = i[3]
+      if i[3] >= min_approval:
+        dictionary["approval"] = True
+      else:
+        dictionary["approval"] = False
+      dictionary["expired_time"] = i[1].__str__()
+
+      temp.append(dictionary)
+      json_object = json.dumps(temp)
+    outfile.write(json_object)
+
+
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
-
 
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
 
+  # Check if the address is on message
   if message.content:
     if message.channel.name in allowed_channels:
       message_words = message.content.split()
@@ -54,4 +94,23 @@ async def on_message(message):
             cooldown_list.append((word, cooldown))
             await message.channel.send(s)
 
+  # Check if an attachment is on the message
+  if message.content:
+    if message.attachments:
+      message_words = message.content.split()
+      for word in message_words:
+        if re.search("^[- a-fA-F0-9]{36}$", word):
+          meme_time = (datetime.datetime.now() + datetime.timedelta(minutes=60))
+          meme_list.append([message, meme_time, word, 0])
+
+@client.event
+async def on_reaction_add(user, reaction):
+  update_list(meme_list)
+  update_json(meme_list)
+
+@client.event
+async def on_raw_reaction_remove(payload):
+  update_list(meme_list)
+  update_json(meme_list)
+    
 client.run(TOKEN)
