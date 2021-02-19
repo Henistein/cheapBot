8fa72316335e6fd62fb4b21a526bfbf3ade8b759
Botha <kristo.botha@debeersgroup.com>
2021-02-19 16:49:08 +0200

    error check and case insensitive commands

--- bot.py
+++ bot.py
@@ -1,31 +1,31 @@
-from scripts import * 
-import discord
-from discord.ext import commands
 import os
-import sys
+import discord
+
 
 # bot stuff after here
-
 PREFIX = '$cheap'
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-		print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
 
 
 @client.event
 async def on_message(message):
 
-  msg = message.content.split(' ')
-  if msg[0] == PREFIX:
-    cmd = msg[1]
-    try:
-      #Checks if script is present in scripts/
-      if msg[1] in list(set([file.split('.')[0] for file in os.listdir('scripts/')][3:])):
-        func = eval(f"{msg[1]}.run")
-        await func(client=client, message=message)
-    except ValueError as v:
-      raise v
+    msg = message.content.split(' ')
+    if msg[0].lower() == PREFIX:
+        if len(msg) < 2:
+            return
+        cmd = msg[1].lower()
+        try:
+            # Checks if script is present in scripts
+            if cmd in list(set([file.split('.')[0] for file in os.listdir('scripts/')][3:])):
+                func = eval(f"{cmd}.run")
+                await func(client=client, message=message)
+        except ValueError as v:
+            raise v
 
 client.run(os.getenv('TOKEN'))
